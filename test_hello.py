beea77c0ea404eed8ce4a87a4f58d2372bf71f3f
Isabella <isa058.w@gmail.com>
2025-07-10 11:26:56 -0400

    finished

--- test_hello.py
+++ test_hello.py
@@ -5,6 +5,38 @@ import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+        self.assertNotEqual(hello.hello(), "bye, world!")
+        self.assertNotEqual(hello.hello(), "oompa loompa")
+
+    def test_add(self):
+        self.assertEqual(hello.add(1, 2), 3)
+        self.assertEqual(hello.add(-1, 5), 4)
+        self.assertNotEqual(hello.add(-10, 5), 90)
+
+    def test_sub(self):
+        self.assertEqual(hello.sub(900, 2), 898)
+        self.assertEqual(hello.sub(0, 0), 0)
+        self.assertNotEqual(hello.sub(2000, 5), 200)
+
+    def test_mul(self):
+        self.assertEqual(hello.mul(-3, -4), 12)
+        self.assertNotEqual(hello.mul(-1, 9), -5)
+        self.assertNotEqual(hello.mul(2, 3), 7)
+
+    def test_div(self):
+        self.assertEqual(hello.div(6, 2), 3)
+        self.assertEqual(hello.div(-10, 2), -5)
+        self.assertNotEqual(hello.div(10, 2), 6)
+        
+
+
+
+
+
+
+
+    
+
 
     # def test_sin(self):
     #     self.assertEqual(hello.sin(0), 0)
