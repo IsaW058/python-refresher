283c7ac5864bedc5697af77ac8964098f922d601
Isabella <isa058.w@gmail.com>
2025-07-13 18:32:34 -0400

    finished to 10.1

--- physics.py
+++ physics.py
@@ -84,10 +84,9 @@ def calculate_angular_acceleration(tau, I):  # tau = torque, I = moment of inert
     tau (float): Torque applied to object in newton-meters.
     I (float): moment of inertia of object in kg * m^2
     """
-
     if I <= 0:
         raise ValueError("moment of inertia must be greater than 0")
-    angular_acceleration = tau * I
+    angular_acceleration = tau / I
     return angular_acceleration
 
 
@@ -104,10 +103,10 @@ def calculate_torque(F_magnitude, F_direction, r):
     torque (float): torque applied to object in newton-meters
     """
 
-    F_radians = math.radians(F_direction)
-    sine_radians = math.sin(F_radians)
-    torque = F_magnitude * sine_radians * r
+    F_radians = math.radians(F_direction) #turns into radians
+    torque = F_magnitude * math.sin(F_radians) * r #cross product r * F
     return torque
+
 
 
 # 7
@@ -119,35 +118,197 @@ def calculate_moment_of_inertia(m, r):
     m (float): mass of  object in kg.
     r (float): distance from the axis of rotation to the center of mass of the object in m.
     Return:
-    m_of_inertai (float): moment of inertia of object in kg * m^2
+    m_of_inertia (float): moment of inertia of object in kg * m^2
     """
     m_of_inertia = m * r**2
     return m_of_inertia
 
 
 # 8
-def calculate_auv_acceleration(F_magnitude, mass):
-    auv_acceleration = F_magnitude / mass
+def calculate_auv_acceleration(F_magnitude, F_angle, mass=100): 
+    """
+    Calculates the acceleration of the AUV in the 2D plane.
+    
+    Parameters:
+    F_magnitude: magnitude of force applied by thruster in N.
+    F_angle: angle of the force applied by the thruster in radians
+    mass: mass of AUV in kg
+    Returns:
+    auv_acceleration in meters/sec^2
+    """
+    if F_angle > 30 or F_angle < -30:
+        raise ValueError("angle between 30 and -30")
+    if F_magnitude < 0 or F_magnitude > 100:
+        raise ValueError("thruster force between 0 and 100N")
+    
+    Fx = (F_magnitude * math.cos(F_angle)) #seaprates force into components
+    Fy = (F_magnitude * math.sin(F_angle)) 
+    force_vector = np.array([Fx, Fy]) #makes it into an array
+    auv_acceleration = force_vector/mass 
     return auv_acceleration
 
 
-def calculate_auv_angular_acceleration(F_magnitude, F_angle):
+def calculate_auv_angular_acceleration(F_magnitude, F_angle, inertia, thruster_distance = 0.5):
+    """
+    Calculates the angular acceleration of the AUV in the 2D plane.
+    
+    Parameters:
+    F_magnitude: magnitude of force applied by thruster in N.
+    F_angle: angle of the force applied by the thruster in radians
+    inertia: moment of intertia of AUV in kg * m^2
+    thruster_distance: distnace from center of mass of AUV to thruster in meters
+    Returns:
+    angular_acceleration in radians/sec^2
+    """
     if F_angle > 30 or F_angle < -30:
-        raise ValueError("input must be between 30 and -30")
+        raise ValueError("angle between 30 and -30")
     if F_magnitude < 0 or F_magnitude > 100:
-        raise ValueError("the thruster force has to be between 0 and 100N")
-    F_radians = math.radians(F_angle)
-    torque = F_magnitude * math.sin(F_radians) * 0.5  # thruster distance is 0.5
-    angular_acceleration = torque * 1  # inertia is 1
+        raise ValueError("thruster force between 0 and 100N")
+    torque = F_magnitude * math.sin(F_angle) * thruster_distance  # thruster distance is 0.5
+    angular_acceleration = torque / inertia
+    
     return angular_acceleration
 
 
 # 9
-def calculate_auv2_acceleration(T, alpha, theta, mass):
-    T = np.array([T1, T2, T3, T4])
-    alpha_radians = math.radians(alpha)  # turns into radians
-    theta_radians = math.radians(theta)  # turns into radians
+def calculate_auv2_acceleration(T, alpha, theta, mass = 100):
+    """
+    Calculates the acceleration AUV in the 2D plane.
+
+    Parameters: 
+    T: an np.ndarray of the magnitudes of the forces applied by the thrusters in N.
+    alpha: angle of the thrusters in radians
+    theta: angle of the AUV in radians
+    mass: mass of the AUV in kilograms
+    Returns:
+    acceleration_vector in meters/sec^2
+    """
+
+    T = np.array(T) #converts T into a numpy array
+
+    angles = np.array([theta+alpha,  # makes array of angles of each thruster in order T1-->T4
+                       theta-alpha,
+                       np.pi+theta+alpha,
+                       np.pi+theta-alpha])
+    
+    counter = 0 #defines variable before being used
+    Fx = 0
+    Fy = 0
+    for angle in angles:
+        Fx += (T[counter] * math.cos(angle)) #for each thruster/angle, separates into compnents x and y and adds them together
+        Fy += (T[counter] * math.sin(angle))
+        counter+=1
+
+    force_vector = np.array([Fx, Fy]) #puts added up x and y components into a force vector
+    acceleration_vector = force_vector/mass
+    return acceleration_vector
 
 
-# acceleration x
-# T1*math.sin(alpha_radians + theta_radians) / mass #acceleration from T1 = F/m
+#9.2
+
+def calculate_auv2_angular_acceleration(T, alpha, l, L, inertia = 100):
+    """
+    Calculates the angular acceleration AUV in the 2D plane.
+
+    Parameters: 
+    T: an np.ndarray of the magnitudes of the forces applied by the thrusters in N.
+    alpha: angle of the thrusters in radians
+    l: the distance from the center of mass of AUV to the thrusters in m 
+    L: the distance from the center of mass of AUV to the thrusters in m
+    intertia: the moment of inertia of the AUV in kg * m^2
+    Returns:
+    angular_acceleration in radians/sec^2
+    """
+    T = np.array(T) #converts T into a numpy array
+    angles = np.array([alpha, # makes array of angles of each thruster in order T1-->T4
+                       -alpha,
+                       np.pi+alpha,
+                       np.pi-alpha])
+    
+    thruster_distance = np.sqrt(l**2 + L**2)
+    x = 0
+    auv2_angular_acceleration = 0
+    while x <4:
+        auv2_angular_acceleration += calculate_auv_angular_acceleration(T[x], angles[x], inertia, thruster_distance)
+        x += 1
+    
+    return auv2_angular_acceleration
+   
+
+#10.1
+def simulate_auv2_motion(T, alpha, l, L, mass, inertia = 100, dt = 0.1, t_final = 10, x0 = 0, y0 = 0, theta0=0):
+    T = np.array(T)
+    t = 0
+    i=1
+    time = []
+    steps = t_final/dt
+
+    x = np.zeros(steps)
+    x[0]=x0 #initial position
+    y = np.zeros(steps)
+    y[0]=y0 #initial position
+    theta = np.zeros(steps)
+    theta[0] = theta0 #initial angle
+    v = np.zeros(steps)
+    omega = np.zeros(steps)
+    a = np.zeros(steps)
+    
+
+    while t < t_final:
+        time.append(t) #time steps of simulation 
+        
+        # make array of angles of each thruster in order T1-->T4
+        angles = np.array([theta[i-1]+alpha,  
+                       theta[i-1]-alpha,
+                       np.pi+theta[i-1]+alpha,
+                       np.pi+theta[i-1]-alpha])
+        
+        #COMPUTE FORCE
+        f_x = np.sum(T * np.cos(angles)) #total forces in the x and y directions, multiplies forces of each thruster w angle components, and adds for all the thrusters
+        f_y = np.sum(T* np.sin(angles))
+
+        #COMPUTE ACCELERATION
+        a_x = f_x/mass #acceleration for each direction
+        a_y = f_y/mass
+        a[i-1] = np.sqrt(a_x**2 + a_y**2) #turns into scalar
+
+        #velocity
+        v_x += a_x * dt # velocity = initial + acceleration * time, time elapsed is always in dt intervals
+        v_y += a_y * dt
+        v[i-1] = np.sqrt(v_x**2 + v_y**2) #turns into scalar
+
+        #position
+        x[i] = x[i-1] + v_x * dt #position at point i = the position at point i-1 + velocity*the amount of time that has passed, and x[1-1] = 0
+        y[i] = y[i-1] + v_y * dt
+
+        #angular acceleration
+        ang_acceleration = calculate_auv2_angular_acceleration(T, alpha, theta[i-1], l, L, inertia)
+        omega[i] = omega[i-1] + ang_acceleration * dt # same as position except for angles
+        theta[i] = theta [i-1] + ang_acceleration * dt
+
+        
+        t += dt
+        i += 1
+    
+    return np.array(time), x, y, theta, v, omega, a
+
+
+# OLD 9.2
+"""T = np.array(T) #converts T into a numpy array
+
+    angles = np.array([alpha, # makes array of angles of each thruster in order T1-->T4
+                       -alpha,
+                       np.pi+alpha,
+                       np.pi-alpha])
+    
+    counter = 0 #defines variables before it's used
+    torque = 0
+
+    for angle in angles:
+        x = (T[counter] * math.cos(angle)) #separates every thruster's force into x and y components
+        y = (T[counter] * math.sin(angle))
+        torque+= (x*l - y*L) #for every thruster's F, its cross producted with distance from center of rotation and added together
+        counter+=1 #repeats for every thruster
+    
+    angular_acceleration = torque / inertia
+    return angular_acceleration"""
--- test_physics.py
+++ test_physics.py
@@ -1,5 +1,20 @@
 import unittest
 import physics
+import math
+import numpy as np
+
+
+def calculate_auv_acceleration(F_magnitude, F_angle, mass=100):
+    if F_angle > 30 or F_angle < -30:
+        raise ValueError("angle between 30 and -30")
+    if F_magnitude < 0 or F_magnitude > 100:
+        raise ValueError("thruster force between 0 and 100N")
+
+    Fx = F_magnitude * math.cos(F_angle)  # seaprates force into components
+    Fy = F_magnitude * math.sin(F_angle)
+    force_vector = np.array([Fx, Fy])  # makes it into an array
+    auv_acceleration = force_vector / mass
+    return auv_acceleration
 
 
 class Test_(unittest.TestCase):
@@ -22,7 +37,7 @@ class Test_(unittest.TestCase):
         self.assertNotEqual(physics.calculate_acceleration(10, 7), 30)
 
     def test_angular_acceleration(self):
-        self.assertEqual(physics.calculate_angular_acceleration(10, 2), 20)
+        self.assertEqual(physics.calculate_angular_acceleration(10, 2), 5)
         self.assertNotEqual(physics.calculate_angular_acceleration(10, 2), 30)
 
     def test_torque(self):
@@ -32,6 +47,31 @@ class Test_(unittest.TestCase):
     def test_moment_of_inertia(self):
         self.assertEqual(physics.calculate_moment_of_inertia(3, 3), 27)
         self.assertNotEqual(physics.calculate_moment_of_inertia(3, 3), 20)
+
+    def test_calculate_auv_acceleration(self):
+        acc = calculate_auv_acceleration(10, math.radians(30), 100)
+        # Expected values
+        a_x = 0.0866025404
+        a_y = 0.050
+        #a_x = (10 * math.cos(math.radians(30))) / 100
+        #a_y = (10 * math.sin(math.radians(30))) / 100
+        self.assertAlmostEqual(acc[0], a_x)
+        self.assertAlmostEqual(acc[1], a_y)
+
+        with self.assertRaises(ValueError):
+            physics.calculate_auv_acceleration(120, math.radians(10))
+        with self.assertRaises(ValueError):
+            physics.calculate_auv_acceleration(10, math.radians(-80))
+
+    def test_calculate_auv_angular_acceleration(self):
+        self.assertAlmostEqual(
+            physics.calculate_auv_angular_acceleration(10, 0.4, 1, 0.5), 1.947091711543253)
+        self.assertAlmostEqual(
+            physics.calculate_auv_angular_acceleration(10, 0.4, 2, 0.5), 0.9735458557716263)
+    
+    def test_calculate_auv2_angular_acceleration(self):
+        self.assertAlmostEqual(physics.calculate_auv2_angular_acceleration([10, 1, 4, 0], 0.12, 5, 4, 1), 3.8326606800413248)
+
 
 
 if __name__ == "__main__":
